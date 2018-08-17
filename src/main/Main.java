1fd6ac5bfd4e25cf99f058f4e9a7a7511d7c2f79
Konstantin Ravvin <kravvin@rocketsoftware.com>
2018-08-17 17:38:59 +0500

    Daily save 2018/08/17

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,30 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import currency.Parser;
+import currency.ParserException;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException, ParserException {
+		
+		String expr;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Parser p = new Parser();
+		
+		for(;;) {
+			System.out.println("Input expression: ");
+			expr = br.readLine();
+			if(expr.isEmpty()) break;
+			try {
+				System.out.println("Result: " + p.evaluate(expr) + "\n");
+			}
+				catch (ParserException ex) {
+			}
+		}
 	}
 
 }
